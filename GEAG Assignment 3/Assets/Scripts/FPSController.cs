1888919cf6ecd321b7e1ac443ee3f236f9a67fd6
ken <heyty005@unisa.edu.au>
2019-10-31 15:37:35 +1030

    Initialized project

---
+++ GEAG Assignment 3/Assets/Scripts/FPSController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float mouseSen;
+    Rigidbody rb;
+    CharacterController controller;
+
+    float xRotation;
+    public bool FlyMode = false;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+        if (FlyMode == true)
+        {
+            rb.useGravity = false;
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerLook();
+        PlayerMove();
+    }
+
+    void PlayerLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSen * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSen * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+       
+    }
+
+    void PlayerMove()
+    {
+        float movementForward = Input.GetAxis("Vertical");
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        
+        if (FlyMode == true)
+        {
+             movement = transform.right * movementHorizontal + Camera.main.transform.forward * movementForward;
+        }
+        else
+        {
+             movement = transform.right * movementHorizontal + transform.forward * movementForward;
+        }
+        controller.Move(movement * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ GEAG Assignment 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float interactRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InteractWithObj();
+    }
+
+    void InteractWithObj()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Collider[] interactables = Physics.OverlapSphere(transform.position, interactRadius);
+            if (interactables[0] != null)
+            {
+                interactables[0].GetComponent<Interactable>().Interact();
+            }
+        }
+    }
+}
